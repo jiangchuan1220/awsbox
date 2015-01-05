ae6abb742ebce21804397fb0c92723400220a0b3
jiangchuan1220 <jiangchuan1220@163.com>
2015-01-05 10:55:40 +0800

    update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ http.createServer(function(request, response) {
     response.writeHeader(200, {"Content-Type": "text/html"});  
     response.write("Hello World!");  
     response.end();  
-}).listen(8080);  
-sys.puts("Server running at http://localhost:8080/");  
+}).listen(80);  
+sys.puts("Server running at http://localhost:80/");  
