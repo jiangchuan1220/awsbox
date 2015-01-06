e97c940bbe9ac5410632c969a552d55525bee215
jiangchuan1220 <jiangchuan1220@163.com>
2015-01-06 15:57:34 +0800

    server.js update

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var sys = require("sys"),  
-    http = require("http");  
-http.createServer(function(request, response) {  
-    response.writeHeader(200, {"Content-Type": "text/html"});  
-    response.write("Hello World!");  
-    response.end();  
-}).listen(8080);  
-sys.puts("Server running at http://localhost:8080/");  
+var http = require('http');
+var fs = require('fs');
+
+
+fs.readFile('./server.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8080);
+});
